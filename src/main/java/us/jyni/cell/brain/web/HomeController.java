55b47e1f3091f4a119c83cef977e160310c23e2a
June Young Ahn <jynius@gmail.com>
2023-03-22 03:21:44 +0900

    Controller 추가

---
+++ src/main/java/us/jyni/cell/brain/web/HomeController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package us.jyni.cell.brain.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author jynius
+ *
+ */
+@Controller
+public class HomeController {
+	
+	@GetMapping("/") 
+	@ResponseBody public String home() {
+		return "hello world!";
+	}
+}
